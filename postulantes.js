82f410a38a2ea686a95bc805d3837d248ce4349f
Ignacio <nachocenturion04@gmail.com>
2024-06-24 12:10:00 -0300

    mejorado

--- postulantes.js
+++ postulantes.js
@@ -1,69 +1,68 @@
 function infocandidatos() {
-
-    const olcandidatos = document.querySelector("#info-del-candidato")
-    const candidatos = JSON.parse(localStorage.getItem("candidatos")) || [];
-
+  const olcandidatos = document.querySelector("#info-del-candidato")
+  const candidatos = JSON.parse(localStorage.getItem("candidatos")) || [];
 
 
-    candidatos.forEach(candidato => {
-        olcandidatos.innerHTML+=`
-           <li class="candidato">
-               <ul>
-                   <li>
-                     ${candidato.nombre}
-                   </li>
 
-                   <li>
-                     ${candidato.telefono}
-                   </li>
+  candidatos.forEach(candidato => {
+    olcandidatos.innerHTML+=`
+      <li class="candidato">
+        <ul>
+          <li>
+            ${candidato.nombre}
+          </li>
 
-                   <li>
-                     ${candidato.gmail}
-                   </li>
+          <li>
+            ${candidato.telefono}
+          </li>
+
+          <li>
+            ${candidato.gmail}
+          </li>
                   
-                   <li>
-                     ${candidato.documento}
-                   </li>
+          li>
+            ${candidato.documento}
+          </li>
 
-                   <li>
-                     ${candidato.pregunta1}
-                   </li>
+          <li>
+            ${candidato.pregunta1}
+          </li>
 
-                   <li>
-                     ${candidato.pregunta2}
-                   </li>
+          <li>
+            ${candidato.pregunta2}
+          </li>
 
-                   <li>
-                     ${candidatopregunta3}
-                   </li>
+          <li>
+            ${candidatopregunta3}
+          </li>
 
-                   <li>
-                     ${candidato.pregunta4}
-                   </li>
+          <li>
+            ${candidato.pregunta4}
+          </li>
 
-                   <li>
-                     ${candidato.pregunta5}
-                   </li>
+          <li>
+            ${candidato.pregunta5}
+          </li>
 
-                   <li>
-                     ${candidato.pregunta6}
-                   </li>
+          <li>
+            ${candidato.pregunta6}
+          </li>
 
-                   <li>
-                     ${candidato.pregunta7}
-                   </li>
+          <li>
+            ${candidato.pregunta7}
+          </li>
 
-                   <li>
-                     ${candidato.curriculum}
-                   </li>
+          <li>
+            ${candidato.curriculum}
+          </li>
 
-                   <li>
-                     ${candidato.video}
-                   </li>
-               </ul>
-           </li>
-        `;
-    });
+          <li>
+            ${candidato.video}
+          </li>
+        </ul>
+      </li>
+    `;
+  });
 }
 
 
